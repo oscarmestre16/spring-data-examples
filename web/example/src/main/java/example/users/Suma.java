eef318308bb37e68872e79683c4ccceaa5d808b6
Oscar Mestre Folch <47212334+oscarmestre16@users.noreply.github.com>
2021-03-10 13:57:03 +0100

    Cambios para Testsuma

---
+++ web/example/src/main/java/example/users/Suma.java
@@ -0,0 +1,14 @@
+package example.users;
+
+public class Suma  {
+	int num1, num2;
+	
+	public Suma(int num1, int num2) {
+		this.num1 = num1;
+		this.num2 = num2;
+	}
+	
+	public int suma() {
+		return this.num1 + this.num2;
+	}
+}
